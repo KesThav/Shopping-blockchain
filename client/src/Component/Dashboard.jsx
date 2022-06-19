02444e1cecf14a965a1b0c5acc91720f22746c68
KesThav <kesigan.thav@gmail.com>
2022-06-19 19:50:36 +0200

    initial commit

--- client/src/Component/Dashboard.jsx
+++ client/src/Component/Dashboard.jsx
@@ -31,16 +31,36 @@ const Dashboard = () => {
     pending,
     releasePayment,
     allProducts,
+    products,
+    loading,
+    setLoading,
   } = useContext(ContextAPI);
 
   useEffect(() => {
+    setLoading(true);
     getPendingTransactions();
     getCurrentUser();
     allProducts();
-  }, [pending, currentUser]);
+  }, [loading]);
+
+  if (products && pending) {
+    for (let i = 0; i < pending.length; i++) {
+      for (let j = 0; j < products.length; j++) {
+        if (pending[i].product_uuid === products[j].uuid) {
+          pending[i].product_uuid = products[j].name;
+        }
+      }
+    }
+  }
   return (
     <RenderIf account={currentUser}>
-      <Box p="10" w="100">
+      <Box
+        style={{
+          boxSizing: 'border-box',
+          padding: '20px',
+          overflowX: 'hidden',
+        }}
+      >
         <Flex direction="row">
           <Stat p="10" borderWidth="1px" w="60">
             <Flex align="center">
@@ -51,20 +71,15 @@ const Dashboard = () => {
                   phone number: {currentUser?.phone_number}
                 </Text>
                 <Text fontSize="md">address: {currentUser?.user_address}</Text>
+                <Flex align="center">
+                  <Text fontSize="lg" mr="4">
+                    CHF
+                    {' ' + currentUser?.balance}
+                  </Text>
+                  <SetBalance />
+                </Flex>
               </Box>
             </Flex>
-          </Stat>
-          <Stat p="10" borderWidth="1px" w="60">
-            <StatLabel mb="2">Current balance</StatLabel>
-            <StatNumber>
-              <Flex align="center">
-                <Text fontSize="lg" mr="4">
-                  CHF
-                  {currentUser?.balance}
-                </Text>
-                <SetBalance />
-              </Flex>
-            </StatNumber>
           </Stat>
         </Flex>
         <Flex w="100vw">
@@ -80,11 +95,11 @@ const Dashboard = () => {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {pending.length === 0
+                  {pending.length !== 0
                     ? pending
                         .filter(p => p.status !== 'Closed')
                         .filter(
-                          p => p.seller_address !== currentUser?.user_address
+                          p => p.buyer_address === currentUser?.user_address
                         )
                         .map(p => (
                           <Tr key={p.product_uuid}>
@@ -117,17 +132,16 @@ const Dashboard = () => {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {pending.length === 0
+                  {pending.length !== 0
                     ? pending
                         .filter(p => p.status === 'Closed')
                         .filter(
-                          p => p.seller_address !== currentUser?.user_address
+                          p => p.buyer_address === currentUser?.user_address
                         )
                         .map(p => (
                           <Tr key={p.product_uuid}>
                             <Td>{p.product_uuid}</Td>
                             <Td>{p.pendingAmount}</Td>
-                            <Td></Td>
                           </Tr>
                         ))
                     : 'No closed transactions'}
